6be360924a3f375b90735571b6118d1bd7d78a19
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-01-10 18:02:41 +0100

    Example using redux Thunk.

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { removeTodo, completeTodo } from './actions'
+import { displayAlert } from './thunks'
 import NewTodoForm from './NewTodoForm'
 import TodoItem from './TodoItem'
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletePressed }) => {
+const TodoList = ({ todos = [], onRemovePressed, onCompletePressed, onDisplayAlertClicked }) => {
   return (
     <>
       <NewTodoForm />
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onRemovePressed={onRemovePressed} onCompletePressed={onCompletePressed} />
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onRemovePressed={onRemovePressed}
+          onCompletePressed={onDisplayAlertClicked}
+        />
       ))}
     </>
   )
@@ -22,6 +28,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (id) => dispatch(removeTodo(id)),
   onCompletePressed: (id) => dispatch(completeTodo(id)),
+  onDisplayAlertClicked: () => dispatch(displayAlert()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
